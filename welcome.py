14d2df2d6da69fcd587951df024a5f0cc465ad26
fortune <fortunefoluso26@gmail.com>
2025-07-15 18:15:44 +0100

    Welcome

---
+++ welcome.py
@@ -0,0 +1,12 @@
+line01 = "********************"# header
+line02 = "*                  *"# re-use
+line03 = "*     WELCOME!     *"# welcome message
+
+print() # Starts a blank line
+print(line01)
+print(line02)
+print(line03)
+print(line02)
+print(line01)
+
+# No errors(\*v*/)
